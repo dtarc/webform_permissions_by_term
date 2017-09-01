f44df7b121a9a8c08b87e0d491fc5e145d0f2b78
Matthew Gill <matthew-gill@3539361.no-reply.drupal.org>
2017-09-01 10:50:02 +0100

    Added basic Config settings

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -4,7 +4,9 @@ namespace Drupal\webform_permissions_by_term\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-
+use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 class SettingsForm extends ConfigFormBase {
 
@@ -28,28 +30,48 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('webform_permissions_by_term.settings');
+
+    $config = \Drupal::config('webform_permissions_by_term.settings.webform_permissions_by_term_vocab');
+
 
     $form = parent::buildForm($form, $form_state);
 
-    $description = "";
+    $vocabularies = Vocabulary::loadMultiple();
+    $vocab_options = [];
 
-    $form['webform_permissions_by_term_vocab'] = [
-    ];
+    if(!empty($vocabularies)) {
 
-    return parent::buildForm($form, $form_state);
+      foreach ($vocabularies as $vocab => $settings) {
+        $vocab_options[$vocab] = $vocab;
+      }
+
+      $form['webform_permissions_by_term_vocab'] = [
+        '#type' => 'select',
+        '#title' => $this->t('Select Vocabulary'),
+        '#options' => $vocab_options,
+        '#default_value' => $config->get('value')
+      ];
+
+    } else {
+      drupal_set_message(t('There are no Taxonomy Vocabularies defined in this Drupal instance. Please define one to use this module.'));
+    }
+
+    return $form;
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+
+
     \Drupal::configFactory()
       ->getEditable('webform_permissions_by_term.settings.webform_permissions_by_term_vocab')
       ->set('value', $form_state->getValue('webform_permissions_by_term_vocab'))
       ->save();
 
     parent::submitForm($form, $form_state);
+
   }
 
 }
